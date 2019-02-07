307f082d6fd5769a3c8a875786386b233c1a05c3
Rubin Peci <rubinpeci@gmail.com>
2019-02-06 20:48:07 -0500

    Done

--- 04_canvas-dvd/animate.js
+++ 04_canvas-dvd/animate.js
@@ -1,7 +1,8 @@
 //Databend - Rubin P. and Wei Wen
 //SoftDev Pd08
 //2019-01-04
-//K03 - They lock us in the tower whenever we get caught
+//K04 - What is it saving the screen from?
+
 
 //variables to hold the canvas and the 2d context
 const canvas = document.getElementById("playground");
@@ -17,9 +18,10 @@ var logo = new Image();
 logo.src = "logo_dvd.jpg"
 
 
-//initializing function that starts the necessary methods and prevents several frames from starting
+//draws the dot
 var drawDot = function()
 {
+	//cancels any previous animation frames, preventing them from "stacking" up
 	window.cancelAnimationFrame(requestID);
 
 	//checks if the radius is greater than the canvas width or less than 0
@@ -69,44 +71,60 @@ var stopIt = function(e)
 	}
 };
 
-var dvdLogoSetup = function(e)
+var dvdLogoSetup = function()
 {
-	window.cancelAnimationFrame(requestID);
+	window.cancelAnimationFrame(requestID);												//cancel existing animations
 
+	//set the width and height of the logo
 	var dvdWidth = 100; 
 	var dvdHeight = 50;
+
+	//set the x and y coordinate to a random int within the canvas
 	var dvdX = Math.floor(Math.random() * (canvas.width - dvdWidth));
 	var dvdY = Math.floor(Math.random() * (canvas.height - dvdHeight));
 
+	//x and y velocity
 	var xVel = 1;
 	var yVel = 1;
 
+	//function inside the setup function - necessary to carry over variables
+	//without having to constantly "setup" and have the logo appear in a random spot
+	//on the canvas
 	var dvdLogo = function()
 	{
+		//clear the entire canvas
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+		//draw the logo with the specified parameters at the x and y coordinate variables
 		ctx.drawImage(logo, dvdX, dvdY, dvdWidth, dvdHeight);
 
-		if (dvdX >= canvas.width || dvdX <= 0)
+		//bouncing physics:
+		//if x + 100 is greater than canvas width, or the x is less than/equal to 0, reverse x velocity
+		//if y + 50 is greater than canvas height, or the y is less than/equal to 0, reverse y velocity
+		if (dvdX + dvdWidth >= canvas.width || dvdX <= 0)
 		{
 			xVel = -1 * xVel;
 		}
-		else if (dvdY >= canvas.height || dvdY <= 0)
+		else if (dvdY + dvdHeight >= canvas.height || dvdY <= 0)
 		{
 			yVel = -1 * yVel;
 		}
 
+		//console.log("x: " + dvdX + "\ny: " + dvdY);	debugging
 
+		//increment/decrement the x and y coordinates by their respective velocities
 		dvdX += xVel;
 		dvdY += yVel;
 
-		window.requestAnimationFrame(dvdLogo);
+		//set the requestID and request an animation frame to run this inner function again
+		requestID = window.requestAnimationFrame(dvdLogo);
 	};
 
+	//run the function we just declared
 	dvdLogo();
 };
 
-//link up the releveant event listeners to the buttons 'circle' and 'stop'
+//link up the releveant event listeners to the buttons 'circle,' 'stop,' and 'dvd'
 document.getElementById('circle').addEventListener('click', drawDot);
 document.getElementById('stop').addEventListener('click', stopIt);
-document.getElementById('dvd').addEventListener('click', dvdLogoSetup);
+document.getElementById('dvd').addEventListener('click', dvdLogoSetup);		//note that it runs the SETUP function, not the logo function
