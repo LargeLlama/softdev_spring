c2f8b369b954c24757636cf4a3d05272bbe8e665
Rubin Peci <rubinpeci@gmail.com>
2019-03-06 14:04:41 -0500

    UwU

---
+++ 08_mongosite/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import os, random
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
+
+@app.route('/')
+def root():
+    return render_template('home.html')
