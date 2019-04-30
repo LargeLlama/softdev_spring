7152614ba812eb6e4715124ba0c8311174abf0b9
Rubin Peci <rubinpeci@gmail.com>
2019-04-30 14:05:35 -0400

    uwu

--- 1x_tmp/hw.py
+++ 1x_tmp/hw.py
@@ -4,4 +4,40 @@ def repeat(string):
     return repeat_inner
 
 r1 = repeat("Hey")
+r2 = repeat("UwU ")
+
 print(r1(3))
+print(r2(5))
+print(repeat(":3 ")(3))
+
+'''
+def outer():
+    x = "foo"
+    def inner():
+        x = "bar"
+    inner()
+    return x
+'''
+
+def outer():
+    x = "foo"
+    def inner():
+        nonlocal x 
+        x = "bar"
+    inner()
+    return x
+
+print(outer())
+
+def make_counter():
+    x = 0
+    def counter():
+        nonlocal x
+        x += 1
+        return x
+    return counter
+
+ctr1 = make_counter()
+print(ctr1())
+print(ctr1())
+print(ctr1())
