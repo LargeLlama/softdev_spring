991d45da09c7db9870c49ec8f8a5d381914b9ab8
Rubin Peci <rubinpeci@gmail.com>
2019-05-02 11:14:49 -0400

    Uwu

--- 2x_tmp/homework.py
+++ 2x_tmp/homework.py
@@ -4,19 +4,37 @@ def old_fib(n):
     if n <= 1:
         return n
     else:
-        return fib(n - 1) + fib(n - 2)
+        return old_fib(n - 1) + old_fib(n - 2)
 
 #test
 print("Old way!")
 print(old_fib(5))
 print(old_fib(10))
+#print(old_fib(100)) takes too long
 
 #faster, new
 def memoize(f):
+    '''This works by storing every previous fib in a dict, and if it isn't there it is added, making the process far faster'''
     memo = {}
     def helper(x):
-        return x #filler
+        nonlocal memo
+        if x in memo:
+            return memo[x]
+        else:
+            memo[x] = f(x)
+            return memo[x]
     return helper
 
+@memoize #memoize is called before fib is
 def fib(n):
-    return n #filler
+    '''Typical fib function '''
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print("new")
+print(fib(5))
+print(fib(10))
+print(fib(100))
+print(fib(40))
